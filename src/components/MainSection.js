bdc7fb6fe14acb05f6b025513e30e070c58118a2
MilkMother <118103942+MilkMother@users.noreply.github.com>
2024-05-27 03:29:51 +0200

    images

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -21,7 +21,12 @@ function MainSection() {
 	return (
 		<div className="main-container">
 			{/* Video background */}
-			<video src="/videos/waves1.mp4" autoPlay loop muted />
+			<video
+				src="./single-page-application/videos/waves1.mp4"
+				autoPlay
+				loop
+				muted
+			/>
 
 			{/* Main title */}
 			<h1>ONE BOTTLE AWAY</h1>
