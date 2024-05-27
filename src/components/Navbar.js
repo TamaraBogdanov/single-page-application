d52d5cf98041a6a1a587110f9501b038333777fb
MilkMother <118103942+MilkMother@users.noreply.github.com>
2024-05-27 05:01:00 +0200

    links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,11 +61,7 @@ function Navbar() {
 							</Link>
 						</li>
 						<li className={"nav-item"}>
-							<Link
-								to="/#/about"
-								className="nav-links"
-								onClick={closeMobileMenu}
-							>
+							<Link to="/about" className="nav-links" onClick={closeMobileMenu}>
 								About
 							</Link>
 						</li>
